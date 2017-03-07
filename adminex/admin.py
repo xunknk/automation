d8f69d7f815c5d5644bc7183c52a81433fb54758
xlcq <420201811@qq.com>
2017-03-07 17:29:01 +0800

    整理代码文件

--- adminex/admin.py
+++ adminex/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
-# from .models import login_gw
+from .models import login_gw
 
 
 # Register your models here.
-'''
+
 @admin.register(login_gw)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
-'''
+	pass
--- adminex/models.py
+++ adminex/models.py
@@ -4,11 +4,9 @@ from django.db import models
 
 
 # Create your models here.
-'''
 class login_gw(models.Model):
-    name = models.CharField(max_length=30)
-    pwd = models.TextField()
+	name = models.CharField(max_length=30)
+	pwd = models.TextField()
 
-    def __str__(self):
-        return self.name
-'''
+	def __str__(self):
+		return self.name
